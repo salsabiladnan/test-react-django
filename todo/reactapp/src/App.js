d200666fef2fd77766e4c7dd8508a251aee0228d
salsabiladnan <salsabilaadnan11@gmail.com>
2023-09-04 23:03:32 +0700

    modify App.js and App.css

--- todo/reactapp/src/App.js
+++ todo/reactapp/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="center-column">
+        
+        <div className="item-row">
+          <span>One</span>
+        </div>
+        
+        <div className="item-row">
+          <span>Two</span>
+        </div>
+        
+        <div className="item-row">
+          <span>Three</span>
+        </div>
+
+      </div>
     </div>
   );
 }
